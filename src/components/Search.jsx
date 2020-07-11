ad006bb2c2760ef7afa86189062db8ade50c0a2d
Pratham kumar <prathamsolera@gmail.com>
2020-07-11 22:38:52 +0530

    Fetch data from github API

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,8 +1,24 @@
 import React from "react";
 import "./Search.css";
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 
 function Search() {
+    const [name, setName] = useState("");
+    const [username, setUsername] = useState("");
+
+    useEffect(() => {
+        fetch("https://api.github.com/users/PrathamKumar14")
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            setData(data)
+        })
+    }, []);
+
+    function setData(data) {
+        setName(data.name);
+        setUsername(data.login);
+    }
 
     return (
         <form className="form" action="">
