416f60030657ffdcc9fceeed63061b586598d0bc
Pratham kumar <prathamsolera@gmail.com>
2020-07-11 18:31:42 +0530

    Add basic template

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -4,7 +4,8 @@ import "./Card.css"
 function Card() {
     return (
         <div className="card">
-
+            <h1 className="name">Pratham Kumar</h1>
+            <div className="line"></div>
         </div>
     )
 }
