38d16075d66756db1a6969e8b5f5123ccf2559a0
Pratham kumar <prathamsolera@gmail.com>
2020-07-16 11:57:27 +0530

    Updates

---
+++ precache-manifest.a7afa918c7eaf5bdb1935cedb65e7675.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5511913284ac817238672aba2c3c325b",
+    "url": "/github-id-card/index.html"
+  },
+  {
+    "revision": "cafb832c2d453a7bfa5d",
+    "url": "/github-id-card/static/css/main.a1a93724.chunk.css"
+  },
+  {
+    "revision": "0637b958340565863ad4",
+    "url": "/github-id-card/static/js/2.12b2df2a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github-id-card/static/js/2.12b2df2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cafb832c2d453a7bfa5d",
+    "url": "/github-id-card/static/js/main.d3f63a6d.chunk.js"
+  },
+  {
+    "revision": "7216b37e11ce4c27c370",
+    "url": "/github-id-card/static/js/runtime-main.5c9878b7.js"
+  }
+]);
